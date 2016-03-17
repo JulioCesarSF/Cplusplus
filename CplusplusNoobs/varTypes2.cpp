05bad5d9f92d295211cadb4dfb563ef6e8cb0291
JulioCesarSF <julio.com@uol.com.br>
2016-03-17 18:28:09 -0300

    Create varTypes2.cpp

---
+++ CplusplusNoobs/varTypes2.cpp
@@ -0,0 +1,19 @@
+int main() { 
+	
+	/* to set a value to a var */
+
+	int number; /* not initialized */
+	int number2 = 10; 
+
+	cout << "Numer2 is: " << number2 << endl;
+
+	std::cin >> number; /* you can use std::cin >>  to get the input from user in console*/
+
+	cout << "Number is: " << number << endl;
+
+	number2 = 30; /* to set a value use "="*/
+
+	cout << "Number2 is: " << number2;
+
+	return 0;
+}
