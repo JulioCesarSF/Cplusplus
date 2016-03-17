532412622a7650db93634a25a2bd634895b23f01
JulioCesarSF <julio.com@uol.com.br>
2016-03-17 18:17:17 -0300

    Create helloWorld.cpp

---
+++ CplusplusNoobs/helloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() { 
+	/* this main is accepted and you don't need a return because the compiler will put a return 0
+	/* the tradicional main in c++ : int main(int argc, char* argv[]) */
+
+	cout << "Hello World!" << endl;
+
+	/* in Visual Studio use CTRL+F5 to run */
+
+	return 0;
+}
