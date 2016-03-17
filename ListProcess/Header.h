750740dbcafeda89eefcd04cf20a463edd48b795
JulioCesarSF <julio.com@uol.com.br>
2016-03-17 15:14:50 -0300

    Create Header.h

---
+++ ListProcess/Header.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class Process {
+
+public:
+
+	bool listProcess() {
+		
+		PROCESSENTRY32 pe32;
+		HANDLE h;
+
+		pe32.dwSize = sizeof(PROCESSENTRY32);
+		h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+		if (!h)
+			return false;
+
+		BOOL have = Process32First(h, &pe32);
+
+		if (have) {
+
+			do{
+				cout << "Process name: " << pe32.szExeFile << endl;
+				have = Process32Next(h, &pe32);
+			} while (have);
+		}
+		else {
+			CloseHandle(h);
+			return false;
+		}	
+
+		return true;
+
+	}
+
+};
