59167b179e7fde6d7dcc230a96c95cb9ef93d4de
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-03-18 13:35:06 -0300

    Update array.cpp

--- CplusplusNoobs/array.cpp
+++ CplusplusNoobs/array.cpp
@@ -5,10 +5,6 @@ using std::endl;
 
 int main() {
 
-	/*
-	Array is like a box where you can store different things in diferent compartiments
-	*/
-	
 	int numbers[10] = {};
 
 	for (int i = 0; i < 10; ++i) {
@@ -19,6 +15,24 @@ int main() {
 		cout << "Item: " << i << " value: " << numbers[i] << endl;
 	}
 
+	/*
+	Array is like a box where you can store different things in diferent compartiments
+		LINES X COLLS
+
+		int number[10][10]  this mean a matrix of 10x10, 10 lines by 10 collums
 	
+	*/
+
+	int n1[10][10] = {};
+
+	n1[3][4] = 5;
+
+	n1[0][0] = 1;
+
+	cout << "Line 3 and Col 4 value: " << n1[3][4] << endl;
+	cout << "Line 0 and Col 0 value: " << n1[0][0] << endl;
+
+
+
 	return 0;
 }
