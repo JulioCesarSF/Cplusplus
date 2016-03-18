6b5436971449426e3be8188dd1c7eae6e381940d
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-03-18 15:40:06 -0300

    Update Source.cpp

--- CreateWindow/Source.cpp
+++ CreateWindow/Source.cpp
@@ -1,5 +1,11 @@
 #include "Header.h"
 
+struct Controls {
+	HWND btn1;
+};
+
+Controls controls;
+
 Window* myWin = new Window();
 
 LRESULT CALLBACK Proc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -7,13 +13,23 @@ LRESULT CALLBACK Proc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
 
 	case WM_CREATE:
 	{
-		MyButton btn1("Teste1", 50, 24, 50, 50, myWin->getHinst(), hwnd);			
+		MyButton btn1("Teste1", 50, 24, 50, 50, myWin->getHinst(), hwnd);
+		controls.btn1 = btn1.getHwnd();
 	}break;
 
 	case WM_DESTROY:
 	{
 		//delete myWin;
 		PostQuitMessage(0);		
+	}break;
+
+	case WM_COMMAND:
+	{		
+		switch (HIWORD(wParam)) {
+		case BN_CLICKED:
+			if ((HWND)lParam == controls.btn1)
+				MessageBox(hwnd, "Hello from btn1", "Button1", MB_OK);
+		}break;
 	}break;
 
 	default:
