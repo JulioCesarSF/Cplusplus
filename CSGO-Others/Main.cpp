1cd20eccfdde2f6e42cd9e86720b2f65f5fdb497
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-04-16 20:49:57 -0300

    Update Main.cpp

--- CSGO-Others/Main.cpp
+++ CSGO-Others/Main.cpp
@@ -3,9 +3,8 @@
 #include "Cheat.h"
 
 
-Cheat* t = new Cheat();
-
 int main() {
+	Cheat* t = new Cheat();
 
 	Window* w = new Window();
 	bool found = false;
@@ -15,27 +14,43 @@ int main() {
 			found = true;	
 		Sleep(1);
 	} while (!found);
-	delete w;
+
 	
 	if (t->openIt("csgo.exe", PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION)) {
 		cout << "Attached!" << endl;
 		t->setAttached(true);
-		t->setClientDll(t->getDllModule("client.dll", "csgo.exe"));
+		do {
+			t->setClientDll(t->getDllModule("client.dll", "csgo.exe"));
+			t->setEngineDll(t->getDllModule("engine.dll", "csgo.exe"));
+			::Sleep(50);
+		} while (t->getEngineDll() == 0 || t->getClientDll() == 0);
 		cout << "client.dll:\t" << t->getClientDll() << endl;
+		cout << "engine.dll:\t" << t->getEngineDll() << endl;
 
-		std::thread trigger = t->callTrigger(true, false);
+		std::thread trigger = t->callTrigger(true, false, true);
 		std::thread noflash = t->callNoFlash();
-		std::thread bunny = t->callBH();	
+		std::thread bunny = t->callBH();
+		std::thread aim = t->callAimbot(true);
 		trigger.detach();
 		noflash.detach();
 		bunny.detach();
+		aim.detach();
+
+		found = false;
 
 		while (true) {
 
-			cout << '\r' << "Trigger:\t" << t->getUseTrigger() << "\tNoFlash:\t" << t->getUseNoFlash() << "\tBunnyhop:\t" << t->getUseBH() << std::flush;
+			cout << '\r' << "Trigger: " << t->getUseTrigger() 
+				<< "\tNoFlash: " << t->getUseNoFlash() 
+				<< "\tBunnyhop: " << t->getUseBH() 
+				<< "\tAimbot: " << t->getUseAimbot() << std::flush;
 
-			if (GetAsyncKeyState(VK_DELETE))
+			if (GetAsyncKeyState(VK_DELETE) || found)
 				break;
+
+			if (!w->isWindowOpen("Valve001"))
+				found = true;
+
 			Sleep(50);
 		}		
 	}
