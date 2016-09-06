1068ebda8174ee3205ba0e0896dacad71e4408c8
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-09-06 15:29:06 -0300

    Create resource.h

---
+++ LobbyFinder_old/resource.h
@@ -0,0 +1,50 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by LFinder.rc
+//
+#define janela1                         101
+#define IDR_MENU1                       103
+#define IDI_ICON1                       106
+#define IDR_HTML1                       107
+#define IDI_ICON2                       114
+#define IDI_ICON3                       115
+#define IDR_WAVE1                       116
+#define IDC_LIST1                       1002
+#define IDC_EDIT1                       1004
+#define IDC_BUTTON1                     1005
+#define ID_LOBB_ADDPROFILE              40001
+#define ID_LOBB_DELETEPROFILE           40002
+#define ID_LOBB_CLOSE                   40003
+#define ID_LIST_SAVELIST                40004
+#define ID_LIST_LOADLIST                40005
+#define ID_CONFIGURATION_AUTOREFRES     40006
+#define ID_CONFIGURATION_AUTOREFRESHTIMER 40007
+#define ID_AUTOREFRESHTIMER_5SECONDS    40008
+#define ID_AUTOREFRESHTIMER_10SECONDS   40009
+#define ID_AUTOREFRESHTIMER_20SECONDS   40010
+#define ID_AUTOREFRESHTIMER_30SECONDS   40011
+#define ID_AUTOREFRESHTIMER_60SECONDS   40012
+#define ID_AUTOREFRESHTIMER_2MINUTES    40013
+#define ID_COPY_COPYPROFILELINK         40014
+#define ID_COPY_COPYLOBBYKEY            40015
+#define ID_CONFIGURATION_PLAYSOUND      40016
+#define ID_PLAYSOUND_SELECTEDLOBBYAVAILABLE 40017
+#define ID_LIST_UPDATELIST              40018
+#define ID_Menu                         40020
+#define ID_LOBBY_CLOSE                  40021
+#define ID_LOBBY_JOINLOBBY              40022
+#define ID_LOBBY_JOINLOBBY40023         40023
+#define ID_LOBBY_CLOSE40024             40024
+#define ID_LOBB_JOIN                    40025
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        117
+#define _APS_NEXT_COMMAND_VALUE         40026
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
